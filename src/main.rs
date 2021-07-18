09a691c2afee1b5e4019dc8098925d78d8eee043
Matheus Battirola <mcbattirola@hotmail.com>
2021-07-18 18:52:22 -0300

    Step 6: 3D!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
-    const IMAGE_WIDTH: usize = 512;
+    const IMAGE_WIDTH: usize = 512 * 2; // * 2 because we display two screens
     const IMAGE_HEIGHT: usize = 512;
 
-    let mut frame_buffer = [255; IMAGE_WIDTH * IMAGE_HEIGHT];
+    let mut frame_buffer = [pack_color(ColorChannel {
+        red: 255,
+        green: 255,
+        blue: 255,
+        alpha: 0,
+    }); IMAGE_WIDTH * IMAGE_HEIGHT];
 
     let map_w = 16;
     let map_h = 16;
@@ -31,22 +36,22 @@ fn main() {
         fov: std::f64::consts::PI / 3 as f64,
     };
 
-    // generate image
-    for j in 0..IMAGE_HEIGHT {
-        for i in 0..IMAGE_WIDTH {
-            let c = ColorChannel {
-                red: (255 * j / IMAGE_HEIGHT) as u8,
-                green: (255 * i / IMAGE_HEIGHT) as u8,
-                blue: 0,
-                alpha: 0,
-            };
+    // // generate image
+    // for j in 0..IMAGE_HEIGHT {
+    //     for i in 0..IMAGE_WIDTH {
+    //         let c = ColorChannel {
+    //             red: (255 * j / IMAGE_HEIGHT) as u8,
+    //             green: (255 * i / IMAGE_HEIGHT) as u8,
+    //             blue: 0,
+    //             alpha: 0,
+    //         };
 
-            frame_buffer[i + j * IMAGE_WIDTH] = pack_color(c);
-        }
-    }
+    //         frame_buffer[i + j * IMAGE_WIDTH] = pack_color(c);
+    //     }
+    // }
 
     // generate map
-    let rect_w = IMAGE_WIDTH / map_w;
+    let rect_w = IMAGE_WIDTH / (map_w * 2);
     let rect_h = IMAGE_HEIGHT / map_h;
     for j in 0..map_h {
         for i in 0..map_w {
@@ -96,28 +101,47 @@ fn main() {
         pack_color(player_color),
     );
 
-    // draw visibility cone
-    for j in 0..IMAGE_WIDTH {
+    // draw visibility cone and the 3D view
+    for j in 0..IMAGE_WIDTH / 2 {
         let angle = player.view_angle as f64 - (player.fov / 2.0)
-            + player.fov as f64 * j as f64 / IMAGE_WIDTH as f64;
+            + player.fov as f64 * j as f64 / (IMAGE_WIDTH / 2) as f64;
         for i in 0..400 {
             let t = i as f64 * 0.05;
             let cx = player.x as f64 + t * angle.cos();
             let cy = player.y as f64 + t * angle.sin();
 
-            if map[cx as usize + cy as usize * map_w] != 32u8 {
-                break;
-            }
-
             let pix_x: usize = (cx * rect_w as f64) as usize;
             let pix_y: usize = (cy * rect_h as f64) as usize;
 
+            // draws the visibility cone
             frame_buffer[pix_x + (pix_y * IMAGE_WIDTH)] = pack_color(ColorChannel {
-                red: 255,
-                green: 255,
-                blue: 255,
+                red: 160,
+                green: 160,
+                blue: 160,
                 alpha: 0,
-            })
+            });
+
+            // if the ray touches a wall, draw a vertical column
+            if map[cx as usize + cy as usize * map_w] != 32u8 {
+                let col_height = IMAGE_HEIGHT as f64 / t;
+                let col_color = ColorChannel {
+                    red: 0,
+                    green: 255,
+                    blue: 255,
+                    alpha: 0,
+                };
+                draw_rectangle(
+                    &mut frame_buffer,
+                    IMAGE_WIDTH,
+                    IMAGE_HEIGHT,
+                    IMAGE_WIDTH / 2 + j,
+                    (IMAGE_HEIGHT as f64 / 2.0 - col_height / 2.0) as usize,
+                    1,
+                    col_height as usize,
+                    pack_color(col_color),
+                );
+                break;
+            }
         }
     }
 
@@ -188,6 +212,11 @@ fn draw_rectangle(
         for j in 0..h {
             let cx = x + i;
             let cy = y + j;
+
+            if cx >= img_w || cy >= img_h {
+                continue;
+            }
+
             image[cx + cy * img_w] = color;
         }
     }
